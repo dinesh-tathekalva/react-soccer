c47eecb75913ca89d2854800549607c35b580f57
Mickey <mickey@Mickeys-MacBook-Pro.local>
2020-08-24 23:58:26 -0500

    upddated

--- src/App.js
+++ src/App.js
@@ -1,74 +1,42 @@
 import React, {Component} from 'react';
 import './App.css';
-import Notes from './components/Notes'
+import Movies from './components/Movies'
 import axios from 'axios'
 
+const years = [2010, 2015, 2016, 2017]
 class App extends Component {
 
-  state ={
-    persons: [],
-    flag: false
-  }
-
-  componentDidMount () {
-    axios.get('https://jsonplaceholder.typicode.com/comments')
-    .then(response => {
-     const emails = response.data.map(i => {
-      return{
-        ...i
-      }
-     }
-      )
-      console.log(emails.email)
-      this.setState({
-        persons: emails
-      })
-   
-    })
-  }
-
-  clickHandler = () => {
-    this.setState ({
-      flag: !this.state.flag
-    })
-  }
-
-  render(){
-    const data = this.state.persons
-    const displayPersons = data.filter(i=>i.email.includes('co.uk')).map((person, key) =>  <Notes email = {person.email} key={person.id}/>)
-    if(this.state.flag){
-      
+  constructor (){
+    super()
+    this.state = {
+      movies: []
     }
-   
+  }
+
+  changeHandler = (e) => {
+    axios.get(`https://jsonmock.hackerrank.com/api/movies?Year=${e.target.value}`)
+    .then(response => {
+      const movieData = response.data.map(i => {
+          return {
+              ...i
+          }
+      })
+    this.setState({
+      movies: movieData
+    })
+      
+
+  })
+  }
+  render (){
+  const optionList = years.map(year => <option key={year}>{year}</option>)
     return (
-      <div className="App">
-
-        
-        {displayPersons}
-
-        <button onClick={this.clickHandler}>Click me</button>
-      </div>
+      <React.Fragment>
+        <Movies options ={optionList} changeYear={this.changeHandler}/>
+        <p>{this.state.movies}</p>
+      </React.Fragment>
     )
   }
+
 }
-
-export default App;
-
-
-
-
-  // {
-    //   id: 1,
-    //   name: 'Dinesh',
-    //   age: 30
-    // },
-    // {
-    //   id: 2,
-    //   name: 'Mohana',
-    //   age: 25
-    // },
-    // {
-    //   id: 3,
-    //   name: 'Theja',
-    //   age: 29
-    // }
+export default App
